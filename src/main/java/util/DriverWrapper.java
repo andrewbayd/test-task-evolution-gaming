a30e5463ee30051013715a57683d0a130eb7469a
Andrii Baidachenko <andrewbayd@gmail.com>
2018-12-07 21:37:00 +0200

    Make class DriverWrapper package-private

--- src/main/java/util/DriverWrapper.java
+++ src/main/java/util/DriverWrapper.java
@@ -3,7 +3,7 @@ package util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DriverWrapper {
+class DriverWrapper {
     private static final String CHROME_DRIVER_PATH = "./src/main/resources/chromedriver";
 
     private static WebDriver getDriver() {
